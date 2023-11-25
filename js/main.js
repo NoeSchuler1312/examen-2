1da9c1fb4cc66bcf1d991e2b946d114cc81e39e5
NoeSchuler1312 <122873508+NoeSchuler1312@users.noreply.github.com>
2023-11-25 14:21:49 +0100

    hover (animations, couleurs...)

--- js/main.js
+++ js/main.js
@@ -12,3 +12,18 @@ var toggleMenu = function toggleMenu(event) {
 
 burger.addEventListener("click", toggleMenu);
 filter.addEventListener("click", toggleMenu);
+
+//musique
+window.addEventListener("DOMContentLoaded", (event) => {
+  const audio = document.querySelector("audio");
+  audio.volume = 0.5;
+  audio.play();
+});
+
+var mute = document.querySelector(".mute");
+
+var toggleMute = function toggleMute(event) {
+  mute.classList.toggle("is-active");
+};
+
+mute.addEventListener("click", toggleMenu);
