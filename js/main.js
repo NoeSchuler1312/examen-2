0c693550588ecd6e3af6cf27706e04c2bd8e38a9
NoeSchuler1312 <122873508+NoeSchuler1312@users.noreply.github.com>
2023-12-06 08:44:16 +0100

    Musique de fond + fondu
    Musique et fondu noir quand on lance le site. Le bouton pour enlever le son est en cours de développement.

--- js/main.js
+++ js/main.js
@@ -13,17 +13,17 @@ var toggleMenu = function toggleMenu(event) {
 burger.addEventListener("click", toggleMenu);
 filter.addEventListener("click", toggleMenu);
 
-//musique
+// musique + bouton pour le son
 window.addEventListener("DOMContentLoaded", (event) => {
   const audio = document.querySelector("audio");
   audio.volume = 0.5;
   audio.play();
 });
 
-var mute = document.querySelector(".mute");
+var mute = document.querySelector(".button-mute");
 
 var toggleMute = function toggleMute(event) {
-  mute.classList.toggle("is-active");
+  mute.classList.toggle("is-off");
 };
 
-mute.addEventListener("click", toggleMenu);
+mute.addEventListener("click", toggleMute);
